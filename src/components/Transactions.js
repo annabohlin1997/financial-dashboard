239f2ae3e192e7dc42341b5aaa4fdd52d4e889f3
annaBohlin <anna@bohlinoffice.se>
2023-01-11 12:38:54 +0100

    added styling to Transactions component

--- src/components/Transactions.js
+++ src/components/Transactions.js
@@ -1,11 +1,16 @@
+import "../styles/Transactions.css";
+
 const Transactions = ({ transactions }) => {
   return (
     <div>
       <ul>
         {transactions.map((transaction, i) => (
-          <li key={i}>
-            <img src={transaction.icon + ".svg"} alt="" /> {transaction.name} {transaction.date} {transaction.category} $
-            {transaction.amount}
+          <li className="transactions-list" key={i}>
+            <img className="transactions-icon" src={transaction.icon + ".svg"} alt="" /> 
+            <p>{transaction.name}</p> 
+            <p className="transactions-light-gray">{transaction.date}</p> 
+            <p className="transactions-light-gray">{transaction.category}</p> 
+            <p className="transactions-amount">{transaction.amount > 0 ? `$${transaction.amount}` : `-$${Math.abs(transaction.amount)}`}</p>
           </li>
         ))}
       </ul>
