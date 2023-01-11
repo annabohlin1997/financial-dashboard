3f841c628ccf41be038acc937f5fbeecea6d8b23
annaBohlin <anna@bohlinoffice.se>
2023-01-11 16:21:49 +0100

    added more styles to SpendingStatistics component

--- src/components/SpendingStatistics.js
+++ src/components/SpendingStatistics.js
@@ -27,24 +27,24 @@ const SpendingStatistics = ({ transactions }) => {
     <div className="statistics-wrapper">
       <img className="statistics-green" src="food-colored.svg" alt="" />
       <div className="statistics-main">
-        <div style={{ backgroundColor: "#F00", width: `${foodRatio * 100}%` }}>
-        _
+        <div className="statistics-bar" style={{ backgroundColor: "#22a447", width: `${foodRatio * 100}%` }}>
+        .
         </div>
         <p>Food</p>
       </div>
       <p className="statistics-percentage">{Math.floor(foodRatio * 100)}%</p>
       <img className="statistics-orange" src="shopping-colored.svg" alt="" />
       <div className="statistics-main">
-        <div style={{ backgroundColor: "#F00", width: `${shoppingRatio * 100}%` }}>
-          _
+        <div className="statistics-bar" style={{ backgroundColor: "#f8964c", width: `${shoppingRatio * 100}%` }}>
+          .
         </div>
         <p>Shopping</p>
       </div>
       <p className="statistics-percentage">{Math.floor(shoppingRatio * 100)}%</p>
       <img className="statistics-blue" src="travel-colored.svg" alt="" />
       <div className="statistics-main">
-        <div style={{ backgroundColor: "#F00", width: `${transportationRatio * 100}%`,}}>
-          _
+        <div className="statistics-bar" style={{ backgroundColor: "#70a6e8", width: `${transportationRatio * 100}%`,}}>
+          .
         </div>
         <p>Transportation</p>
       </div>
