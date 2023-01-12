96b899285f3e3231e0a637e264ebb53e82ec4e44
Staffan Ösp <staffan.osp@gmail.com>
2023-01-12 14:14:40 +0100

    move dummyData to separate file

---
+++ src/helpers/dummyData.js
@@ -0,0 +1,56 @@
+export const transactions = [
+  {
+    id: 5,
+    date: "2023-01-01",
+    name: "Uber",
+    amount: -32.2,
+    category: "transportation",
+  },
+  {
+    id: 4,
+    date: "2023-01-01",
+    name: "Max",
+    amount: -16.23,
+    category: "food",
+  },
+  {
+    id: 3,
+    date: "2022-12-30",
+    name: "Systembolaget",
+    amount: -65.96,
+    category: "food",
+  },
+  {
+    id: 2,
+    date: "2022-12-29",
+    name: "H&M",
+    amount: -112.14,
+    category: "shopping",
+  },
+
+  {
+    id: 1,
+    date: "2022-12-25",
+    name: "Salary",
+    amount: 2000,
+    category: "income",
+  },
+];
+
+export const goals = [
+  {
+    date: "2023-04-01",
+    name: "MacBook Pro",
+    amount: 2500,
+  },
+  {
+    date: "2023-06-02",
+    name: "Taormina",
+    amount: 5000,
+  },
+  {
+    date: "2025-01-01",
+    name: "House",
+    amount: 2000000,
+  },
+];
