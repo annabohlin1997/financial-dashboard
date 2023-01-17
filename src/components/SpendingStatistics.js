05fd1e09167b9c0faa8b2fb74d58c370cdcdc3be
Staffan Ösp <staffan.osp@gmail.com>
2023-01-17 12:29:02 +0100

    refactor animation to a separate function

--- src/components/SpendingStatistics.js
+++ src/components/SpendingStatistics.js
@@ -2,93 +2,86 @@ import { useEffect, useRef, useState } from "react";
 import { clamp, smootherstep } from "../helpers/mathHelpers";
 import "../styles/SpendingStatistics.css";
 
+const animate = ({ animTimeMs, animations, refCallBack }) => {
+  const animStartTime = Date.now();
+
+  const animFrame = () => {
+    const animProgress = clamp((Date.now() - animStartTime) / animTimeMs, 0, 1);
+
+    refCallBack(
+      requestAnimationFrame(() => {
+        for (let animation of animations) {
+          animation.animCallBack(
+            smootherstep(animProgress, animation.animStartV, animation.animEndV)
+          );
+        }
+
+        if (animProgress < 1) {
+          animFrame();
+        }
+      })
+    );
+  };
+
+  animFrame();
+};
+
 const SpendingStatistics = ({ transactions }) => {
   const [foodRatio, setFoodRatio] = useState(0);
   const [shoppingRatio, setShoppingRatio] = useState(0);
   const [transportationRatio, setTransportationRatio] = useState(0);
 
   const requestAnimationFrameRef = useRef();
-  const animStartTime = useRef();
-  const animTimeMs = 800;
-
-  const animFoodRatioStart = useRef();
-  const animFoodRatioEnd = useRef();
-  const animShoppingRatioStart = useRef();
-  const animShoppingRatioEnd = useRef();
-  const animTransportationRatioStart = useRef();
-  const animTransportationRatioEnd = useRef();
 
   useEffect(() => {
     cancelAnimationFrame(requestAnimationFrameRef.current);
-
-    //animation starts here
-    animStartTime.current = Date.now();
 
     const sumOfExpenses = transactions
       .filter((transaction) => transaction.amount < 0)
       .reduce((sum, transaction) => sum + transaction.amount, 0);
 
-    animFoodRatioStart.current = foodRatio;
-    animFoodRatioEnd.current =
+    const newFoodRatio =
       transactions
         .filter((transaction) => transaction.category === "food")
         .reduce((sum, transaction) => sum + transaction.amount, 0) /
       sumOfExpenses;
 
-    animShoppingRatioStart.current = shoppingRatio;
-    animShoppingRatioEnd.current =
+    const newShoppingRatio =
       transactions
         .filter((transaction) => transaction.category === "shopping")
         .reduce((sum, transaction) => sum + transaction.amount, 0) /
       sumOfExpenses;
 
-    animTransportationRatioStart.current = transportationRatio;
-    animTransportationRatioEnd.current =
+    const newTransportationRatio =
       transactions
         .filter((transaction) => transaction.category === "transportation")
         .reduce((sum, transaction) => sum + transaction.amount, 0) /
       sumOfExpenses;
 
-    requestAnimationFrameRef.current = requestAnimationFrame(animate);
+    animate({
+      animTimeMs: 800,
+      refCallBack: (ref) => (requestAnimationFrameRef.current = ref),
+      animations: [
+        {
+          animStartV: foodRatio,
+          animEndV: newFoodRatio,
+          animCallBack: setFoodRatio,
+        },
+        {
+          animStartV: shoppingRatio,
+          animEndV: newShoppingRatio,
+          animCallBack: setShoppingRatio,
+        },
+        {
+          animStartV: transportationRatio,
+          animEndV: newTransportationRatio,
+          animCallBack: setTransportationRatio,
+        },
+      ],
+    });
 
     return () => cancelAnimationFrame(requestAnimationFrameRef.current);
   }, [transactions]);
-
-  const animate = () => {
-    const animProgress = clamp(
-      (Date.now() - animStartTime.current) / animTimeMs,
-      0,
-      1
-    );
-
-    setFoodRatio(
-      smootherstep(
-        animProgress,
-        animFoodRatioStart.current,
-        animFoodRatioEnd.current
-      )
-    );
-
-    setShoppingRatio(
-      smootherstep(
-        animProgress,
-        animShoppingRatioStart.current,
-        animShoppingRatioEnd.current
-      )
-    );
-
-    setTransportationRatio(
-      smootherstep(
-        animProgress,
-        animTransportationRatioStart.current,
-        animTransportationRatioEnd.current
-      )
-    );
-
-    if (animProgress < 1) {
-      requestAnimationFrameRef.current = requestAnimationFrame(animate);
-    }
-  };
 
   return (
     <div className="statistics-wrapper">
